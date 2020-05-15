0b9c70fbf76ce9503bb5918aa52cc1c719808a49
Amr-Aboshama <31421825+Amr-Aboshama@users.noreply.github.com>
2020-05-16 00:03:20 +0200

    Adding Dependencies & Libraries

---
+++ Server/src/main/java/Crawler.java
@@ -0,0 +1,15 @@
+
+import org.jsoup.Jsoup;
+
+
+public class Crawler {
+
+	public static void main(String[] args) {
+		System.out.println("Crawler");
+	}
+	
+	public Crawler() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
--- Server/src/main/java/Indexer.java
+++ Server/src/main/java/Indexer.java
@@ -3,6 +3,10 @@ import opennlp.tools.stemmer.PorterStemmer;
 
 public class Indexer {
 
+	public static void main(String[] args) {
+		System.out.println("Indexer");
+	}
+	
 	public Indexer() {
 		// TODO Auto-generated constructor stub
 	}
