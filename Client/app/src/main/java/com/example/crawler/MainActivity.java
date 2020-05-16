1d97e4c3a54c2e5242e8436bee01c5b3ade41418
Ayman Azzam <aymanazzam63@gmail.com>
2020-05-16 11:25:53 +0200

    Adding widget SearchView to MainActivity

--- Client/app/src/main/java/com/example/crawler/MainActivity.java
+++ Client/app/src/main/java/com/example/crawler/MainActivity.java
@@ -7,6 +7,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.SearchView;
 
 import com.loopj.android.http.AsyncHttpClient;
 import com.loopj.android.http.AsyncHttpResponseHandler;
@@ -25,9 +26,9 @@ import java.net.URL;
 import cz.msebera.android.httpclient.Header;
 import opennlp.tools.stemmer.PorterStemmer;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements SearchView.OnQueryTextListener {
 
-    EditText edit_text;
+    SearchView search_view;
     Button button;
 
     @Override
@@ -35,7 +36,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        edit_text = (EditText)(findViewById(R.id.search_phrase));
+        search_view = (SearchView)(findViewById(R.id.search_phrase));
         button = (Button) findViewById(R.id.crawler_search);
 
         /************* Pressing Search-Button Action *************/
@@ -47,6 +48,8 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(i);
             }
         });
+
+        search_view.setOnQueryTextListener(this);
     }
 
     @Override
@@ -54,10 +57,24 @@ public class MainActivity extends AppCompatActivity {
         super.onPause();
 
         /************* Test Steaming *************/
-        System.out.println(edit_text.getText());
+        System.out.println(search_view.getQuery());
         PorterStemmer porterStemmer = new PorterStemmer();
-        String stem = porterStemmer.stem(edit_text.getText().toString());
-        System.out.println("The stem of " + edit_text.getText().toString() + " is " + stem);
+        String stem = porterStemmer.stem(search_view.getQuery().toString());
+        System.out.println("The stem of " + search_view.getQuery().toString() + " is " + stem);
 
+    }
+
+    /************* Pressing Search Action *************/
+    @Override
+    public boolean onQueryTextSubmit(String query) {
+        Intent i = new Intent(MainActivity.this,ResultsActivity.class);
+        i.putExtra("EXTRA_PAGE_NUMBER", "0");
+        startActivity(i);
+        return false;
+    }
+
+    @Override
+    public boolean onQueryTextChange(String newText) {
+        return false;
     }
 }
