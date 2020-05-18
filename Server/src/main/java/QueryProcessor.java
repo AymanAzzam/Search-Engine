d685087f1970334fdd57cd3771a3a2a84e65cb76
Ayman Azzam <aymanazzam63@gmail.com>
2020-05-18 13:03:56 +0200

    Done Steaming

--- Server/src/main/java/QueryProcessor.java
+++ Server/src/main/java/QueryProcessor.java
@@ -1,4 +1,3 @@
-
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
@@ -12,18 +11,65 @@ public class QueryProcessor {
 	        System.out.println("Query Processor started\n");
 	        
 	        /*** Declare Variables ***/
-	        ArrayList<String> stopWords = new ArrayList<String>();
-	        String sentence ="\"how to install nodejs\"" ;
-	        boolean phraseSearch = false;
+	        String sentence ="\"how to install nodejs on linux\"" ;
+	        
+	    	/*** Converting the Sentence into words ***/
+	    	ArrayList<String> queryWords = query(sentence);
+	    	
+	     }
+	 
+	 	public static ArrayList<String> query(String sentence) throws FileNotFoundException
+	 	{
+	        String phraseSearch = "0";
 	        
 	        /*** Checking Phrase Search ***/
 	    	if(sentence.charAt(0) == '"' && sentence.charAt(sentence.length()-1) == '"')
-	    		 phraseSearch = true;
+	    		 phraseSearch = "1";
+	    	
+	    	/*** Converting the Sentence into words ***/
+	    	ArrayList<String> queryWords = steaming(sentence);
+	    	queryWords.add(0,phraseSearch);
 	    	
 	    	/*** For Testing Purpose ***/
-	    	System.out.println("Checking Phrase Search = "+phraseSearch+"\n");
+	        System.out.println("\nAfter Query: ");
+	        for(String word: queryWords)	System.out.println(word);
+	    	
+	    	return queryWords;
+	 	}
+	     
+	 	public static ArrayList<String> steaming(String sentence) throws FileNotFoundException
+	 	{
+	 		/*** Declare Variables ***/
+	 		ArrayList<String> stopWords = new ArrayList<String>();
+	    	 
+	 		/*** Removing the Speacial Charachters ***/
+	    	sentence = sentence.replaceAll("[^a-zA-Z0-9 ]", "");
+	    	
+	    	/*** Converting the Sentence into words ***/
+	    	ArrayList<String> queryWords = new ArrayList<String>(Arrays.asList(sentence.split(" ")));
+	    	
+	        /*** Reading the Stop Words ***/
+	        File file = new File("stopwords.txt");
+	        Scanner scanner = new Scanner(file);
+	        while (scanner.hasNextLine()) { stopWords.add(scanner.nextLine());  }
+	        scanner.close();
 	        
-	        /*** Removing the Speacial Charachters ***/
+	        /*** Deleting the Stop Words ***/
+	        for(String word : stopWords)	queryWords.remove(word);
+	        
+	        /*** Steaming ***/ 
+	        PorterStemmer porterStemmer = new PorterStemmer();
+	        for(int i =0; i < queryWords.size(); i++)	queryWords.set(i, porterStemmer.stem(queryWords.get(i)));
+	        
+	        return queryWords;	 
+	 	}
+	 	
+	 	private static ArrayList<String> testSteaming(String sentence) throws FileNotFoundException
+	 	{
+	 		/*** Declare Variables ***/
+	 		ArrayList<String> stopWords = new ArrayList<String>();
+	    	 
+	 		/*** Removing the Speacial Charachters ***/
 	    	sentence = sentence.replaceAll("[^a-zA-Z0-9 ]", "");
 	    	
 	    	/*** For Testing Purpose ***/
@@ -34,38 +80,29 @@ public class QueryProcessor {
 	    	
 	    	/*** For Testing Purpose ***/
 	        System.out.println("After Converting the Sentence into words: ");
-	        for(String word: queryWords)
-	            System.out.println(word);
+	        for(String word: queryWords)	System.out.println(word);
+	        
+	        /*** Reading the Stop Words ***/
+	        File file = new File("stopwords.txt");
+	        Scanner scanner = new Scanner(file);
+	        while (scanner.hasNextLine()) { stopWords.add(scanner.nextLine());  }
+	        scanner.close();
 	        
 	        /*** Deleting the Stop Words ***/
-	        for(String word : readStopWords("stopwords.txt"))	queryWords.remove(word);
+	        for(String word : stopWords)	queryWords.remove(word);
 	        
 	        /*** For Testing Purpose ***/
 	        System.out.println("\nAfter Deleting the Stop Words: ");
-	        for(String word: queryWords)
-	            System.out.println(word);
+	        for(String word: queryWords)	System.out.println(word);
 	        
-	        /*** Steaming ***/
+	        /*** Steaming ***/ 
 	        PorterStemmer porterStemmer = new PorterStemmer();
-	        for(int i =0; i < queryWords.size(); i++)
-	        	queryWords.set(i, porterStemmer.stem(queryWords.get(i)));
+	        for(int i =0; i < queryWords.size(); i++)	queryWords.set(i, porterStemmer.stem(queryWords.get(i)));
 	        
 	        /*** For Testing Purpose ***/
 	        System.out.println("\nAfter Steaming: ");
-	        for(String word: queryWords)
-	            System.out.println(word);
+	        for(String word: queryWords)	System.out.println(word);
 	        
-	     }
-	     
-	     private static ArrayList<String> readStopWords(String directory) throws FileNotFoundException
-	     {
-	         ArrayList<String> stopWords = new ArrayList<String>();
-	         
-	         File file = new File(directory);
-	         Scanner scanner = new Scanner(file);
-	         while (scanner.hasNextLine()) { stopWords.add(scanner.nextLine());  }
-	         scanner.close();
-	         
-	         return stopWords;
-	     }
+	        return queryWords;	 
+	 	}
 }
