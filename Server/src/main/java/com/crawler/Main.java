0287f5fb519c2c3d67c8736a7cc2c82edcb2219d
Amr-Aboshama <31421825+Amr-Aboshama@users.noreply.github.com>
2020-06-03 12:52:05 +0200

    Fixing Processing and Ranking code

--- Server/src/main/java/com/crawler/Main.java
+++ Server/src/main/java/com/crawler/Main.java
@@ -2,16 +2,18 @@ package com.crawler;
 
 import java.io.FileNotFoundException;
 import java.sql.Connection;
+import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.Hashtable;
+import javax.servlet.http.*;
 
-public class Main {
-
+public class Main extends HttpServlet{
+	
 	 public static void main(String []args) throws FileNotFoundException,Exception {
 	       		
 			//Receiving Request called query it's type is query
 			
-			String query = "Received Request" ;
+			String query = "Test Query" ;
 			ArrayList<String> queryWords, invertedFileElement, linkFileElement;
 			
 			/********** Abo Shama Should update the following Two lines **************/ 
@@ -29,35 +31,105 @@ public class Main {
 				ArrayList<WordValue> invertedFileTempList = new ArrayList<WordValue> ();
 
 				invertedFileElement = dbController.getInvertedFile(conn,queryWords.get(i));
+
 				for(int j=0; j<invertedFileElement.size(); j+=4)
 				{
-					linkFileElement = dbController.getUrlFile(conn,invertedFileElement.get(j));
+					linkFileElement = dbController.getUrlFile(conn,Integer.parseInt(invertedFileElement.get(j)));
 					invertedFileElement.set(j,linkFileElement.get(1));
-
 					
 					WebsiteValue websiteValue = new WebsiteValue(Integer.parseInt(linkFileElement.get(3)), linkFileElement.get(0), linkFileElement.get(2));
 					linkDatabase.put(linkFileElement.get(1), websiteValue);
 					
-					WordValue wordvalue = new WordValue(invertedFileElement.get(j+1), Integer.parseInt(invertedFileElement.get(j+4)), Integer.parseInt(invertedFileElement.get(j+2)));
+					WordValue wordvalue = new WordValue(invertedFileElement.get(j), Integer.parseInt(invertedFileElement.get(j+3)), Integer.parseInt(invertedFileElement.get(j+1)));
 					invertedFileTempList.add(wordvalue);
 				}
 				
 				invertedFile.put(queryWords.get(i), invertedFileTempList);
 			}
 			
-			Integer dummyTotalNumberOfDocuments = 3;
-
+			Integer dummyTotalNumberOfDocuments = dbController.getURLsSize(conn);
+			
+			
+			ArrayList<OutputValue> result;
 			if(queryWords.get(0) == "1")
 			{
 				query = query.replaceAll("[^a-zA-Z0-9 ]", "");
 				PhraseSearch phSearch = new PhraseSearch(invertedFile, linkDatabase, dummyTotalNumberOfDocuments, query);
-				ArrayList<OutputValue> result = phSearch.phraseSearch();
+				result = phSearch.phraseSearch();
 	
 			}
 			else
 			{
 				Ranker ranker = new Ranker (invertedFile, linkDatabase, dummyTotalNumberOfDocuments);
-				ArrayList<OutputValue> result = ranker.rank();
+				result = ranker.rank();
+			}
+			
+			for(OutputValue o:result) {
+				System.out.println(o.getWebsiteName() + "\n" + o.getHeaderText() + "\n" + o.getSummary());
 			}
 	     }
+	 
+	 public void doGet(HttpServletRequest request, HttpServletResponse response) throws FileNotFoundException {
+
+			//Receiving Request called query it's type is query
+			
+			String query = request.getParameter("Query");
+			ArrayList<String> queryWords, invertedFileElement, linkFileElement;
+			
+			/********** Abo Shama Should update the following Two lines **************/
+			DBController dbController;
+			Connection conn;
+			
+			try {
+				dbController = new DBController();
+				conn = dbController.connect();
+				queryWords = QueryProcessor.query(query);
+				Hashtable<String, WebsiteValue> linkDatabase = new Hashtable <String, WebsiteValue> ();
+				Hashtable<String, ArrayList<WordValue>> invertedFile = new Hashtable <String, ArrayList<WordValue>> ();
+				
+				for(int i=1; i<queryWords.size(); i++)
+				{
+					
+					ArrayList<WordValue> invertedFileTempList = new ArrayList<WordValue> ();
+					
+					invertedFileElement = dbController.getInvertedFile(conn,queryWords.get(i));
+					for(int j=0; j<invertedFileElement.size(); j+=4)
+					{
+						linkFileElement = dbController.getUrlFile(conn,Integer.parseInt(invertedFileElement.get(j)));
+						invertedFileElement.set(j,linkFileElement.get(1));
+						
+						
+						WebsiteValue websiteValue = new WebsiteValue(Integer.parseInt(linkFileElement.get(3)), linkFileElement.get(0), linkFileElement.get(2));
+						linkDatabase.put(linkFileElement.get(1), websiteValue);
+						
+						WordValue wordvalue = new WordValue(invertedFileElement.get(j+1), Integer.parseInt(invertedFileElement.get(j+4)), Integer.parseInt(invertedFileElement.get(j+2)));
+						invertedFileTempList.add(wordvalue);
+					}
+					
+					invertedFile.put(queryWords.get(i), invertedFileTempList);
+				}
+				
+				Integer dummyTotalNumberOfDocuments = 3;
+				
+				
+				if(queryWords.get(0) == "1")
+				{
+					query = query.replaceAll("[^a-zA-Z0-9 ]", "");
+					PhraseSearch phSearch = new PhraseSearch(invertedFile, linkDatabase, dummyTotalNumberOfDocuments, query);
+					ArrayList<OutputValue> result = phSearch.phraseSearch();
+					
+				}
+				else
+				{
+					Ranker ranker = new Ranker (invertedFile, linkDatabase, dummyTotalNumberOfDocuments);
+					ArrayList<OutputValue> result = ranker.rank();
+				}
+			} catch (Exception e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			
+
+		 
+	 }
 }
