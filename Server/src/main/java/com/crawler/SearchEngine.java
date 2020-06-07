4cfce7bae5f5d6b27a6e1e4e25584edfad8e307b
Amr-Aboshama <31421825+Amr-Aboshama@users.noreply.github.com>
2020-06-08 00:59:05 +0200

    Added Popularity To SearchEngine

--- Server/src/main/java/com/crawler/SearchEngine.java
+++ Server/src/main/java/com/crawler/SearchEngine.java
@@ -6,6 +6,7 @@ import java.sql.Connection;
 import java.util.ArrayList;
 import java.util.Hashtable;
 import javax.servlet.http.*;
+import javax.sound.midi.ControllerEventListener;
 
 import org.json.JSONArray;
 
@@ -14,65 +15,135 @@ public class SearchEngine extends HttpServlet{
 	final static int MAX_RESULTS = 100;
 	
 	
-//	public static void main(String []args) throws FileNotFoundException,Exception {
-//       		
-//		//Receiving Request called query it's type is query
-//		
-//		String query = "Test Query" ;
-//		ArrayList<String> queryWords, invertedFileElement, linkFileElement;
-//		
-//		/********** Abo Shama Should update the following Two lines **************/ 
-//		DBController dbController = new DBController();
-//		Connection conn = dbController.connect();
-//		
-//		queryWords = query(query);
-//		
-//		Hashtable<String, WebsiteValue> linkDatabase = new Hashtable <String, WebsiteValue> ();
-//		Hashtable<String, ArrayList<WordValue>> invertedFile = new Hashtable <String, ArrayList<WordValue>> ();
-//		
-//		for(int i=1; i<queryWords.size(); i++)
-//		{
-//			
-//			ArrayList<WordValue> invertedFileTempList = new ArrayList<WordValue> ();
-//		
-//			invertedFileElement = dbController.getInvertedFile(conn,queryWords.get(i));
-//		
-//			for(int j=0; j<invertedFileElement.size(); j+=4)
-//			{
-//				linkFileElement = dbController.getUrlFile(conn,Integer.parseInt(invertedFileElement.get(j)));
-//				invertedFileElement.set(j,linkFileElement.get(1));
-//				
-//				WebsiteValue websiteValue = new WebsiteValue(Integer.parseInt(linkFileElement.get(3)), linkFileElement.get(0), linkFileElement.get(2));
-//				linkDatabase.put(linkFileElement.get(1), websiteValue);
-//				
-//				WordValue wordvalue = new WordValue(invertedFileElement.get(j), Integer.parseInt(invertedFileElement.get(j+3)), Integer.parseInt(invertedFileElement.get(j+1)));
-//				invertedFileTempList.add(wordvalue);
-//			}
-//			
-//			invertedFile.put(queryWords.get(i), invertedFileTempList);
-//		}
-//		
-//		Integer dummyTotalNumberOfDocuments = dbController.getURLsSize(conn);
-//		
-//		
-//		ArrayList<OutputValue> result;
-//		if(queryWords.get(0) == "1")
-//		{
-//			query = query.replaceAll("[^a-zA-Z0-9 ]", "");
-//			PhraseSearch phSearch = new PhraseSearch(invertedFile, linkDatabase, dummyTotalNumberOfDocuments, query);
-//			result = phSearch.phraseSearch();
-//		
-//		}
-//		else
-//		{
-//			Ranker ranker = new Ranker (invertedFile, linkDatabase, dummyTotalNumberOfDocuments);
-//			result = ranker.rank();
-//		}
-//		
-//		for(OutputValue o:result) {
-//			System.out.println(o.getWebsiteName() + "\n" + o.getHeaderText() + "\n" + o.getSummary());
-//		}
-//     }
+	public static void main(String []args) throws FileNotFoundException,Exception {
+			  
+		
+		//Receiving Request called query it's type is query
+		
+		try {
+			
+			//Receiving Request called query it's type is query
+			
+			String query = "youtube" ;
+			
+			int type = 0;
+			
+			ArrayList<String> queryWords, invertedFileElement, linkFileElement;
+			
+			/********** Abo Shama Should update the following Two lines **************/ 
+			DBController dbController = new DBController();
+			Connection conn = dbController.connect();
+			
+			queryWords = query(query);
+			
+			Hashtable<String, WebsiteValue> linkDatabase = new Hashtable <String, WebsiteValue> ();
+			Hashtable<String, ArrayList<WordValue>> invertedFile = new Hashtable <String, ArrayList<WordValue>> ();
+			
+			for(int i=1; i<queryWords.size(); i++)
+			{
+				
+				ArrayList<WordValue> invertedFileTempList = new ArrayList<WordValue> ();
+				
+				invertedFileElement = dbController.getInvertedFile(conn,queryWords.get(i));
+				
+				for(int j=0; j<invertedFileElement.size(); j+=4)
+				{
+					linkFileElement = dbController.getUrlFile(conn,Integer.parseInt(invertedFileElement.get(j)));
+					invertedFileElement.set(j,linkFileElement.get(1));
+					
+					WebsiteValue websiteValue = new WebsiteValue(Integer.parseInt(linkFileElement.get(3)), linkFileElement.get(0), linkFileElement.get(2));
+					linkDatabase.put(linkFileElement.get(1), websiteValue);
+					
+					WordValue wordvalue = new WordValue(invertedFileElement.get(j), Integer.parseInt(invertedFileElement.get(j+3)), Integer.parseInt(invertedFileElement.get(j+1)));
+					invertedFileTempList.add(wordvalue);
+				}
+				
+				invertedFile.put(queryWords.get(i), invertedFileTempList);
+			}
+			
+			Integer dummyTotalNumberOfDocuments = dbController.getURLsSize(conn);
+			
+
+			if(!Ranker.donePopularity) {
+				int siz = dbController.getCrawlingSize(conn);
+				if(siz == Main.MAX_LINKS_CNT) {
+					Ranker.donePopularity = true;
+					
+					Hashtable<String, ArrayList<String>> pointingWebsites = new Hashtable<String, ArrayList<String>>();
+					Hashtable<String, Integer> pointedToCount = new Hashtable<String, Integer>();
+					
+					ArrayList<String> URLs = dbController.getAllURLs(conn);
+
+					for(String url:URLs) {
+						pointingWebsites.put(url, dbController.getPointedFromURLs(conn, url));
+						pointedToCount.put(url, dbController.getPointingToCount(conn, url));
+					}
+					
+					Ranker.calculatePopularity(pointingWebsites, pointedToCount);
+				}
+			}
+
+			
+			
+			Object result;
+			
+			
+			
+		    JSONArray json;
+			if(queryWords.get(0) == "1")
+			{
+				query = query.replaceAll("[^a-zA-Z0-9 ]", "");
+				PhraseSearch phSearch = new PhraseSearch(invertedFile, linkDatabase, dummyTotalNumberOfDocuments, query);
+				result = phSearch.phraseSearch();
+				
+				json = new JSONArray((ArrayList<OutputValue>)result);
+			}
+			else
+			{
+				Ranker ranker = new Ranker (invertedFile, linkDatabase, dummyTotalNumberOfDocuments, type);
+				result = ranker.rank(conn, dbController);
+				
+				if(type == 0) {
+					
+					ArrayList<OutputValue> tmp = ((ArrayList<OutputValue>)result);
+					for(int i=1 ; i<queryWords.size() ; ++i) {
+						ArrayList<OutputValue> ret = dbController.getMatchingURLs(conn, queryWords.get(i));
+						tmp.addAll(0, ret);
+					}
+
+					int siz = tmp.size();
+					for(int i=0 ; i<siz ; ++i) {
+						for(int j=i+1 ; j<siz ; ++j) {
+							if(tmp.get(i).getWebsiteName().equals(tmp.get(j).getWebsiteName())) {
+								// System.out.println(tmp.get(i).getWebsiteName() + " " + tmp.get(j).getWebsiteName());
+								tmp.remove(j);
+								--j;
+								--siz;
+							}
+						}
+					}
+
+					json = new JSONArray((ArrayList<OutputValue>)result);
+				}
+				else {
+					
+					json = new JSONArray((ArrayList<OutputImageValue>)result);
+				}
+			}
+			
+			conn.close();
+			
+			for(OutputValue o:(ArrayList<OutputValue>)result) {
+				// System.out.println(o.getWebsiteName() + "\n" + o.getHeaderText() + "\n" + o.getSummary());
+				System.out.println(o.getWebsiteName());
+			}
+			
+		} catch (Exception e) {
+			// TODO: handle exception
+			e.printStackTrace();
+		}
+		
+    }
 	
 	public static ArrayList<String> query(String sentence) throws FileNotFoundException,Exception
  	{
@@ -135,13 +206,32 @@ public class SearchEngine extends HttpServlet{
 			
 			Integer dummyTotalNumberOfDocuments = dbController.getURLsSize(conn);
 			
-			conn.close();
+
+			if(!Ranker.donePopularity) {
+				int siz = dbController.getCrawlingSize(conn);
+				if(siz == Main.MAX_LINKS_CNT) {
+					Ranker.donePopularity = true;
+					
+					Hashtable<String, ArrayList<String>> pointingWebsites = new Hashtable<String, ArrayList<String>>();
+					Hashtable<String, Integer> pointedToCount = new Hashtable<String, Integer>();
+					
+					ArrayList<String> URLs = dbController.getAllURLs(conn);
+
+					for(String url:URLs) {
+						pointingWebsites.put(url, dbController.getPointedFromURLs(conn, url));
+						pointedToCount.put(url, dbController.getPointingToCount(conn, url));
+					}
+
+					Ranker.calculatePopularity(pointingWebsites, pointedToCount);
+				}
+			}
+
 			
 			
 			Object result;
 			
-
-
+			
+			
 		    response.setContentType("application/json");
 		    response.setCharacterEncoding("UTF-8");
 		    JSONArray json;
@@ -159,14 +249,34 @@ public class SearchEngine extends HttpServlet{
 				result = ranker.rank(conn, dbController);
 				
 				if(type == 0) {
+					
+					ArrayList<OutputValue> tmp = ((ArrayList<OutputValue>)result);
+					for(int i=1 ; i<queryWords.size() ; ++i) {
+						ArrayList<OutputValue> ret = dbController.getMatchingURLs(conn, queryWords.get(i));
+						tmp.addAll(0, ret);
+					}
+					
+					int siz = tmp.size();
+					for(int i=0 ; i<siz ; ++i) {
+						for(int j=i+1 ; j<siz ; ++j) {
+							if(tmp.get(i).getWebsiteName().equals(tmp.get(j).getWebsiteName())) {
+								// System.out.println(tmp.get(i).getWebsiteName() + " " + tmp.get(j).getWebsiteName());
+								tmp.remove(j);
+								--j;
+								--siz;
+							}
+						}
+					}
+					
 					json = new JSONArray((ArrayList<OutputValue>)result);
 				}
 				else {
-
+					
 					json = new JSONArray((ArrayList<OutputImageValue>)result);
 				}
 			}
-
+			
+			conn.close();
 			response.getWriter().print(json);
 			
 		} catch (Exception e) {
